ceaf17947b55f5b52ae3127ffc6334cd4ecb4ae0
asmahmed1 <aahmed52@students.kennesaw.edu>
2025-11-21 18:17:27 -0500

    Create rules engine for matching skin types to products

---
+++ engine/rules.py
@@ -0,0 +1,39 @@
+RULES = {
+  "dry": [
+      "gentle cleanser",
+      "hyaluronic acid serum",
+      "ceramide moisturizer",
+      "spf 30+"
+  ],
+  "oily": [
+      "gel cleanser",
+      "niacinamide serum",
+      "lightweight moisturizer",
+      "spf 30+"
+  ],
+  "combination": [
+      "gentle cleanser",
+      "niacinamide or HA serum",
+      "balanced moisturizer",
+      "spf 30+"
+  ],
+  "normal": [
+      "gentle cleanser",
+      "basic moisturizer",
+      "spf 30+"
+  ],
+  "sensitive": [
+      "fragrance free cleanser",
+      "soothing serum",
+      "ceramide moisturizer",
+      "mineral spf"
+  ]
+}
+
+CONCERN_ADDONS = {
+  "acne": ["salicylic acid 2% (pm, alternate days)"],
+  "dryness": ["occlusive at night (pm)"],
+  "redness": ["azelaic acid 10% (pm, alternate days)"],
+  "dark-spots": ["vitamin c (am) or azelaic acid (pm)"],
+  "dullness": ["gentle exfoliation 1 to 2 times per week"]
+}
